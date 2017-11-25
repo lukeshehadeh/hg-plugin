b1aecfe0f902a960231e0d5c2d4611e7344ba8ce
Ian Shehadeh <IanShehadeh2020@gmail.com>
2017-11-25 22:41:04 +0000

    added json output to items.py

--- util/items.py
+++ util/items.py
@@ -21,10 +21,24 @@ class Item(DB.Entity):
     starter_rarity = orm.Required(float)
     nourishment = orm.Optional(float)
 
-"""
-main is the main body of the script
-"""
+def item_to_obj(item):
+    """item_to_obj takes an Item and removed name and numeric_id"""
+    i = dict()
+    i["minecraft_id"] = item.minecraft_id
+    i["starter_rarity"] = item.starter_rarity
+    i["rarity"] = item.starter_rarity
+    if item.nourishment is not None:
+        i["nourishment"] = item.nourishment
+    if item.power is not None:
+        i["power"] = item.power
+    return i
+def item_list_to_map(items):
+    """item_list_to_map takes and item list and uses the item's name as a key"""
+    return {i.name: item_to_obj(i) for i in items}
+
+
 def main():
+    """main is the main body of the script"""
     if len(sys.argv) < 3:
         print("please specify a database file and action")
         exit(1)
@@ -33,12 +47,10 @@ def main():
     if sys.argv[2] == "add":
         items = None
         if len(sys.argv) > 3:
-            print("adding items from file \"{}\"".format(sys.argv[3]))
-            with open(sys.argv[3]) as f:
-                items = json.load(f)
+            with open(sys.argv[3]) as json_file:
+                items = json.load(json_file)
             DB.commit()
         else:
-            print("adding items from stdin")
             items = json.load(sys.stdin)
         with orm.db_session:
             for name, item in items["items"].items():
@@ -48,6 +60,17 @@ def main():
                     item["nourishment"] = None
                 Item(minecraft_id=item['minecraft_id'], name=name, power=item['power'], rarity=item['rarity'],
                  starter_rarity=item['starter_rarity'], nourishment=item['nourishment'])
+    elif sys.argv[2] == "dump":
+        dumpfile = None
+        if len(sys.argv) > 3:
+            print("dumping items to file \"{}\"".format(sys.argv[3]))
+            dumpfile = open(sys.argv[3])
+        else:
+            dumpfile = sys.stdout
+        with orm.db_session:
+            items = orm.select(i for i in Item)
+            json.dump({"items": item_list_to_map(items)}, dumpfile)
+            dumpfile.write("\n")
     else:
         print("unknown command '" + sys.argv[1] +"'")
         exit(1)
